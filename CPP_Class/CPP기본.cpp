a328cde29909b3df452c7152317b21f56fe5eee7
dinmoy <imhyojin614@gmail.com>
2023-03-16 10:08:42 +0900

    개행을 해주는 endl객체

--- CPP_Class/CPP기본.cpp
+++ CPP_Class/CPP기본.cpp
@@ -3,7 +3,8 @@ int main()
 {
 	//std::cout : 출력을 위한 객체
 	//<< : 출력 연산자
-	std::cout << "Hello World\n";
+	//std::endl : 개행(줄바꿈) 객체 - endline의 약자
+	std::cout << "Hello World"<< std::endl;
 
 	return 0;
 }
