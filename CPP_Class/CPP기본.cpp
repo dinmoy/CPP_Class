84f421c099b1a38c0e5c9b6a3e31d0ff2d1ffa5b
dinmoy <imhyojin614@gmail.com>
2023-03-16 10:05:39 +0900

    C++ 스타일 : 출력

--- CPP_Class/CPP기본.cpp
+++ CPP_Class/CPP기본.cpp
@@ -1,5 +1,9 @@
-#include <stdio.h>
-int main() {
-	printf("Hello World\n");
+#include <iostream>
+int main() 
+{
+	//std::cout : 출력을 위한 객체
+	//<< : 출력 연산자
+	std::cout << "Hello World\n";
+
 	return 0;
 }
