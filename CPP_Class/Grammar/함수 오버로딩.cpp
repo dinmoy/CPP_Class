b192cf1354229e99ccb93bba517ea5baad0e7525
dinmoy <imhyojin614@gmail.com>
2023-03-21 10:01:44 +0900

    float은 숫자 뒤에 f를 붙이자

--- CPP_Class/Grammar/함수 오버로딩.cpp
+++ CPP_Class/Grammar/함수 오버로딩.cpp
@@ -1,17 +1,17 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-int sum_int(int a, int b)
+//함수 오버로딩을 해서 함수 이름을 간결하게 할 수 있다.
+int sum(int a, int b)
 {
 	return a + b;
 }
-float sum_float(float a, float b) {
+float sum(float a, float b) {
 	return a + b;
 }
 int main(void)
 {
-	cout << sum_int(1, 2) << endl;
-	cout << sum_float(1.1, 2.2) << endl;
+	cout << sum(1, 2) << endl;
+	cout << sum(1.1f, 2.2f) << endl;
 	return 0;
 }
