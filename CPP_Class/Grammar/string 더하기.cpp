c48c4db2d4479d89d86892621b5935e25fe5125e
dinmoy <imhyojin614@gmail.com>
2023-03-23 11:01:35 +0900

    C스타일 문자열 합치기

---
+++ CPP_Class/Grammar/string 더하기.cpp
@@ -0,0 +1,20 @@
+﻿//C
+#include <stdio.h>
+#include <string.h>
+//C++
+#include <iostream>
+#include <string>
+int main(void)
+{
+	char str1[30] = "Jiwoo";
+	char str2[30] = "Rock";
+
+	const int  str1_l=strlen(str1);
+	const int  str2_l = strlen(str2);
+	for (int i = str1_l; i < str1_l + str2_l; i++) {
+		str1[i] = str2[1 - str1_l];
+	}
+	printf("%s\n", str1);
+
+	return 0;
+}
