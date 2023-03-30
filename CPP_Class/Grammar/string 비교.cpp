c0ce7203912cf71e2c3416828c69037e1b9dd783
dinmoy <imhyojin614@gmail.com>
2023-03-30 10:02:56 +0900

    구조체 기본

--- CPP_Class/Grammar/string 비교.cpp
+++ CPP_Class/Grammar/string 비교.cpp
@@ -1,28 +1,13 @@
-﻿//C
-#include <stdio.h>
-#include <string.h>
-//C++
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
-int main(void)
-{
 
-	string str1 = "Hello";
-	string str2 = "World";
-
-	//C++ string 편리
-	//str2가 str1보다 사전(dictionary)에 더 늦게 나옴(클 수록 늦게 나옴)--시험
-	if (str2 > str1) {
-		cout << "str2 > str1";
-	}
-	else if (str2 < str1) {
-		cout << "str2<str1";
-	}
-	else {
-		cout << "str2==str1";
-	}
-	
-	return 0;
+int main() {
+    string str1 = "Hello";
+    string str2 = "Hello";
+    
+    if (str1 == str2) cout << str1 + " 같다 " + str2 << endl;
+    else cout << str1 + " 다르다 " + str2 << endl;
+    return 0;
 }
