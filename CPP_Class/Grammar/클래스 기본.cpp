59d791e032efc9c3d3a402f913716bea7f9d52d8
dinmoy <imhyojin614@gmail.com>
2023-04-04 09:12:16 +0900

    매개변수가 없는 생성자 만들기

--- CPP_Class/Grammar/클래스 기본.cpp
+++ CPP_Class/Grammar/클래스 기본.cpp
@@ -7,7 +7,29 @@ using namespace std;
 //구조체,클래스는 일반적으로 단어의 첫 글자를 대문자로 한다
 //class는 default가 private(struct는 default가 public)--시험
 class Student {
+
 public:
+    //생성자(constructor) : 객체가 생성될 때 호출되는 함수
+    //생성자를 정의하지 않으면 자동(default)으로 매개변수가 없는 생성자가 정의된다
+    //Student(){ }
+    Student() {
+        name = "임효진";
+        student_num = 2117;
+        age = 18;
+        gender = 1;
+        department = "소프트웨어과";
+
+    }
+    //class는 멤버변수를 가질 수 있다--멤버함수
+    void print(void)
+    {
+        cout << "이름: " << name << endl;
+        cout << "학번: " << student_num << endl;
+        cout << "나이: " << age << endl;
+        cout << "성별(0)남자,(1)여자: " << gender << endl;
+        cout << "학번: " << department << endl;
+    }
+private:
     string name;
     //시험X: 학번을 성능때문에 뮨자열로 하지 않음
     //일반적으로 문자열은 정수데이터보다 많은 메모리 공간을 요구하며
@@ -18,32 +40,12 @@ public:
     //0:남자 1:여자
     int gender; 
     string department;
-
-    //생성자(constructor) : 객체가 생성될 때 호출되는 함수
-    //생성자를 정의하지 않으면 자동(default)으로 매개변수가 없는 생성자가 정의된다
-    //Student(){ }
-    
-    //class는 멤버변수를 가질 수 있다--멤버함수
-    void print(void) 
-    {
-        cout << "이름: " << name << endl;
-        cout << "학번: " << student_num << endl;
-        cout << "나이: " << age << endl;
-        cout << "성별(0)남자,(1)여자: " << gender << endl;
-        cout << "학번: " << department << endl;
-    }
 };
 
 int main() {
     
     //(struct drink)자료형(C++에서는 drink만 써도  됨)/struct 자체가 자료형
-    Student stu1;
-    stu1.name = "임효진";
-    stu1.student_num = 2117;
-    stu1.age = 18;
-    stu1.gender = 1;
-    stu1.department = "소프트웨어과";
-
+    Student stu1=Student();
     stu1.print();
 
     return 0;
