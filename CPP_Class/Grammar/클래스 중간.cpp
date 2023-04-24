4a1e37c7df0299c5834807afc016869a77a600f6
dinmoy <imhyojin614@gmail.com>
2023-04-25 08:50:42 +0900

    생성자 소멸자 호출되는지 확인

--- CPP_Class/Grammar/클래스 중간.cpp
+++ CPP_Class/Grammar/클래스 중간.cpp
@@ -10,12 +10,14 @@ public:
 		//필요한 길이만큼 문자열 동적할당
 		c_str_ = new char[str_length+1];  //'/0' (널문자) 공간 +1
 		strcpy(c_str_, str);
+		cout << "MString 생성자 호출 시작" << endl;
 	}
 	//소멸자(destructor)
 	~MString() 
 	{
 		//생성자에서 동적할당한 공간을 해제
 		delete[] c_str_;
+		cout << "MString 생성자 호출 완료" << endl;
 	}
 	unsigned int size() { return size_; }
     char* c_str() { return c_str_;  }
