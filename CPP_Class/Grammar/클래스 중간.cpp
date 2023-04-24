33ca4321bcd8a4e1079e97dddbb739dc49a2f0da
dinmoy <imhyojin614@gmail.com>
2023-04-25 08:47:39 +0900

    소멸자로 동적할당한 공간 해제

--- CPP_Class/Grammar/클래스 중간.cpp
+++ CPP_Class/Grammar/클래스 중간.cpp
@@ -11,11 +11,17 @@ public:
 		c_str_ = new char[str_length+1];  //'/0' (널문자) 공간 +1
 		strcpy(c_str_, str);
 	}
-	void print() {
-		cout<<c_str_<<endl;
+	//소멸자(destructor)
+	~MString() 
+	{
+		//생성자에서 동적할당한 공간을 해제
+		delete[] c_str_;
 	}
 	unsigned int size() { return size_; }
     char* c_str() { return c_str_;  }
+	void print() {
+		cout << c_str_ << endl;
+	}
 private:
 	unsigned int size_;	  //문자열의 길이
 	char* c_str_; //문자열을 가리키는 주소
