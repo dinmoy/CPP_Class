4c36dcb77410da3159045168c9005f421597687f
dinmoy <imhyojin614@gmail.com>
2023-05-09 10:00:32 +0900

    정적할당으로 변경

--- CPP_Class/Grammar/클래스 중간.cpp
+++ CPP_Class/Grammar/클래스 중간.cpp
@@ -32,17 +32,7 @@ private:
 int main(void)
 {
 
-	//new로 동적할당한 공간은 반드시 delete로 해제시켜야함
-	//만약 그렇지 않으면 메모리가 누수됨
-	MString* str = new MString("강아지 뇽뇽");
-	cout << str->c_str() << endl;
-	//str에 대한 delete는 진행 되었으나, str->c_str에 대한 delete가 진행되지 않음
-	//TODO: 이를 delete해주는 소멸자(destructor)구현하기
-	delete str;
-
-
-
-
-
+	MString str = MString("Aitai");
+	cout<<str.c_str() << endl;
 	return 0;
 }
