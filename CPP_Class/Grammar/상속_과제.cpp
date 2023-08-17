1ac0b36879550f38c7104a0a074cd80df6a0392b
dinmoy <hyojin614@naver.com>
2023-08-17 10:31:41 +0900

    템플릿 적용 전

---
+++ CPP_Class/Grammar/상속_과제.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Shape {
+private:
+	int point_count_;
+	int x_;
+	int y_;
+public:
+	Shape(int point_count,int x,int y)
+		:point_count_(point_count),x_(x),y_(y)
+	{
+
+	}
+	int get_point_count(int point_count) {
+		return point_count;
+	}
+	void  get_position(int x,int y) {
+		x_ = x;
+		y_ = y;
+	}
+	void draw() {
+		cout << "Shape draw" << endl;
+	}
+	virtual ~Shape() {}
+};
+class RectangleShape : public Shape {
+private:
+	int width_;
+	int height_;
+
+	RectangleShape(int point_count,int x,int y,int width,int height)
+		: Shape(point_count,x,y),width_(width), height_(height)
+	{
+	}
+	void draw() {
+		cout << "Rectangle Shape draw" << endl;
+	}
+};
+class CircleShape : public Shape {
+private:
+	int radius_;
+	CircleShape(int point_count, int x, int y, int radius) 
+	:Shape(point_count,x,y),radius_(radius)
+	{
+	}
+	void draw() {
+		cout << "CIrcle Shape draw" << endl;
+	}
+};
+int main() {
+
+}
