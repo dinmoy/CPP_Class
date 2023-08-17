64da848ca4ed49d81d66265c1a78984f0c1b8ea7
dinmoy <hyojin614@naver.com>
2023-08-17 11:13:04 +0900

    클래스에 템플릿 적용

---
+++ CPP_Class/Grammar/템플릿 과제.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+
+T sum(T a, T b)
+{
+	T z = a + b;
+	return z;
+}
+int main() {
+	cout << sum<int>(3, 5) << endl;
+	cout << sum<double>(4.f, 6.f) << endl;
+}
