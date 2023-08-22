ae8bcbe6ad51f06b19a56949805cf0220def8be1
dinmoy <hyojin614@naver.com>
2023-08-22 09:01:53 +0900

    템플릿 예제 템플릿 적용 전

---
+++ CPP_Class/Grammar/템를릿02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Character {
+public:
+	string  name_;
+	int health_;
+	int attack_;
+	int defence_;
+
+	Character(string name, int health, int attack, int defence)
+		:name_(name), health_(health), attack_(attack), defence_(defence)
+	{
+	}
+	void print() {
+		cout <<"이름: "<< name_ << endl;
+		cout <<"체력: "<< health_ << endl;
+		cout <<"공격: "<<attack_ << endl;
+		cout <<"방어: "<<defence_ << endl;
+	}
+};
+void main() {
+	Character ch("강아지", 100, 20, 10);
+	ch.print();
+}
