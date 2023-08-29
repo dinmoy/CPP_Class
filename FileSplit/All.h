4e82bf5c496315a05562c2f2903a49a77df001bb
dinmoy <hyojin614@naver.com>
2023-08-29 09:58:22 +0900

    만능헤더파일(비추천)

---
+++ FileSplit/All.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include "Animal.h" //따로 지정한 헤더 파일은 <>가 아닌 ""
+#include "Shark.h"
+#include "Whale.h"
+using namespace std;
--- FileSplit/main.cpp
+++ FileSplit/main.cpp
@@ -1,10 +1,4 @@
-﻿#include <iostream>
-#include "Animal.h" //따로 지정한 헤더 파일은 <>가 아닌 ""
-#include "Shark.h"
-#include "Whale.h"
-using namespace std;
-
-
+﻿#include "All.h"
 
 int main(void) 
 {
