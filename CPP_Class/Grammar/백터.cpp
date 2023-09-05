534abf7080bf795745c996a995bcc68f9c92d824
dinmoy <imhyojin614@gmail.com>
2023-09-05 10:02:33 +0900

    백터는 크기가 자유롭게 변한다

--- CPP_Class/Grammar/백터.cpp
+++ CPP_Class/Grammar/백터.cpp
@@ -1,27 +1,24 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(void) {
+int main(void) 
+{
+	//전통적인 배열은 크기가 고정되어있으나 
+	//백터는 크기가 자유롭게 변할 수 있는 동적배열
+	vector<int> arr;
 	int *arr = new int[4];
-	arr[0] = 10, arr[1] = 20, arr[2] = 30, arr[3] = 40;
+	arr.reserve(4);   //백터의 크기를 4로 한다
+	arr.push_back(10);
+	arr.push_back(20);
+	arr.push_back(30);
+	arr.push_back(40);
+
 	// 4개가 꽉 찬 공간에 데이터 하나를 더 집어넣어보자
-	//크기가 더ㅋ 큰 공간을 만든다
-	int* temp = new int[8];
-	for (int i = 0; i < 4; i++)
-		temp[i] = arr[i];
-	//기존 할당된 공간을 지운다
-	delete[] arr;
-	//새 공간으로 갈아탄다.
-	arr = temp;
-	//새로운 데이터를 집어 넣는다.
-	arr[4] = 50;
+	arr.push_back(50);
 
 	for (int i = 0; i < 5; i++)
 		cout << arr[i] << " ";
-
-	//메모리 누수를 막기 위해 동적할당된 메모리는 모두 지워주자
-	delete[] arr;
-
-
+	 
 }
