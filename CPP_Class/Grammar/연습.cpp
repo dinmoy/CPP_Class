8eac0dc37e46d1c7757508a9f213c6ccfa3c7678
dinmoy <imhyojin614@gmail.com>
2023-10-14 23:27:34 +0900

    test commit

---
+++ CPP_Class/Grammar/연습.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	
+	/*map<string, string> pet;
+
+	pet["강아지"] = "말티즈";
+	pet["고양이"] = "페르시안";
+	pet["여  우"] = "북극여우";
+
+	map<string, string>::iterator iter;
+
+	for (iter = pet.begin(); iter != pet.end(); iter++) {
+		cout << iter->first <<"  "<< iter->second << endl;
+	}*/
+
+	vector<string>pet;
+	pet.reserve(4);
+	pet.push_back("강아지");
+	pet.push_back("고양이");
+	pet.push_back("햄스터");
+	pet.push_back("여우");
+
+	vector<string>::iterator iter;
+	for (iter = pet.begin(); iter != pet.end(); iter++) {
+		cout <<*iter << endl;
+	}
+	    
+}
