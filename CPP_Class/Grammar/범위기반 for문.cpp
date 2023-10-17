c7d1b394d05d39f42365e10166bfc2ce4ae29d08
dinmoy <imhyojin614@gmail.com>
2023-10-17 09:59:32 +0900

    auto : 자료형 자동설정

--- CPP_Class/Grammar/범위기반 for문.cpp
+++ CPP_Class/Grammar/범위기반 for문.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
 	double arr[4] = { 1.1,2.2,3.3,4.4 };
-	for (double x : arr)
+
+	//a의 자료형은 너무 명확하게 int라는 걸 알 수 있다
+	auto a = 4;
+
+	//x의 자료형은 너무 명확하게 double이라는 걸 알 수 있다
+	for (auto x : arr)
 		cout << x << endl;
 	return 0;
 }
